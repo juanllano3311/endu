251713988290c76165a6245bb149e419e941fb5b
juanllano3311 <70298836+juanllano3311@users.noreply.github.com>
2020-12-17 08:51:38 -0500

    solucion sdk

--- app/src/main/java/com/example/endulsar/logueado.java
+++ app/src/main/java/com/example/endulsar/logueado.java
@@ -2,10 +2,7 @@ package com.example.endulsar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.ImageView;
 
 import com.google.firebase.firestore.FirebaseFirestore;
 
@@ -16,29 +13,9 @@ public class logueado extends AppCompatActivity {
     public static final String correo="corre;";
     public static final String nombre="nombre";
 
-    ImageView tortica;
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_logueado);
-
-        tortica = findViewById(R.id.imgtoritcas);
-
-
-        tortica.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(getApplicationContext(),listarTortica.class));
-            }
-        });
-
-
-
-
-
-
-
     }
 }
