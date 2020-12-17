e5f26725d5268c297683691eb86dfbdcb373cc6f
juanllano3311 <70298836+juanllano3311@users.noreply.github.com>
2020-12-17 14:01:14 -0500

    pedido

---
+++ app/src/main/java/com/example/endulsar/pedido.java
@@ -0,0 +1,14 @@
+package com.example.endulsar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class pedido extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pedido);
+    }
+}
