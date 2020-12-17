d226518c30eb194e3af0454552ef38d3543c5532
alejandro montoya <alejandromg0805@gmail.com>
2020-12-16 22:47:37 -0500

    recyclerview tortica
    no lo he probado, mi android no agrega el implement del iu de firestore.

--- app/src/main/java/com/example/endulsar/logueado.java
+++ app/src/main/java/com/example/endulsar/logueado.java
@@ -2,7 +2,10 @@ package com.example.endulsar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.google.firebase.firestore.FirebaseFirestore;
 
@@ -13,9 +16,29 @@ public class logueado extends AppCompatActivity {
     public static final String correo="corre;";
     public static final String nombre="nombre";
 
+    ImageView tortica;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_logueado);
+
+        tortica = findViewById(R.id.imgtoritcas);
+
+
+        tortica.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),listarTortica.class));
+            }
+        });
+
+
+
+
+
+
+
     }
 }
